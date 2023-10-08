82b258dff24983d2b0d08ab98cc305774f0b710a
GabrielBPM <gabrielbpmachado@gmail.com>
2023-10-08 15:30:49 -0300

    Correção no urls, retirado um urlpatterns errado

--- scavenv/sca/aplic/urls.py
+++ scavenv/sca/aplic/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
-]
 urlpatterns = [
     path("", views.index, name="index"),
 ]
