2299691578e9bcf463a45e002e874c7f4b9af1e4
GabrielBPM <gabrielbpmachado@gmail.com>
2023-10-08 18:39:00 -0300

    Adicionei SuperUsuário, adicionei classe, dei run

--- scavenv/sca/aplic/admin.py
+++ scavenv/sca/aplic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Produto
 # Register your models here.
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'descricao')  # Campos exibidos na lista
+    list_filter = ('categoria',)  # Filtros na lateral
+    search_fields = ('nome', 'descricao')  # Campo de pesquisa
