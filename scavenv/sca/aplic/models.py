9d330d919280d88896b363ba1c1572bc66cf4a9a
GabrielBPM <gabrielbpmachado@gmail.com>
2023-10-09 00:39:50 -0300

    Correção do models onde havia 2 imports

--- scavenv/sca/aplic/models.py
+++ scavenv/sca/aplic/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 # Create your models here.
 
-from django.db import models
-
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
     saldo_conta = models.DecimalField(max_digits=10, decimal_places=2)
