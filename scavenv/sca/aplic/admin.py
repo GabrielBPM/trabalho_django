91840de179cc7cffc11b8b9178e1ae3081b724e8
GabrielBPM <gabrielbpmachado@gmail.com>
2023-10-09 00:36:01 -0300

    Criadas as classes do projeto a partir do diagrama

--- scavenv/sca/aplic/admin.py
+++ scavenv/sca/aplic/admin.py
@@ -1,8 +1,59 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Pessoa, PessoaFisica, PessoaJuridica, Telefone, Usuario, Cartao, Pedido, HistoricoStatus, Status, InformacaoEnvio, Endereco, ItemPedido, Produto, Categoria
 # Register your models here.
 
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'saldo_conta')
+
+@admin.register(PessoaFisica)
+class PessoaFisicaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cpf', 'data_nascimento', 'sexo', 'rg')
+
+@admin.register(PessoaJuridica)
+class PessoaJuridicaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cnpj', 'titulo_estabelecimento', 'codigo_descricao_atividade_economica')
+
+@admin.register(Telefone)
+class TelefoneAdmin(admin.ModelAdmin):
+    list_display = ('pessoa', 'numero')
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('pessoa', 'email', 'perfil', 'numero_usuario')
+
+@admin.register(Cartao)
+class CartaoAdmin(admin.ModelAdmin):
+    list_display = ('pessoa', 'numero_cartao', 'nome_impresso', 'data_vencimento')
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('pessoa', 'data', 'valor_total', 'valor_desconto')
+
+@admin.register(HistoricoStatus)
+class HistoricoStatusAdmin(admin.ModelAdmin):
+    list_display = ('pedido', 'data')
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(InformacaoEnvio)
+class InformacaoEnvioAdmin(admin.ModelAdmin):
+    list_display = ('pedido', 'valor_frete', 'forma_envio', 'prazo_entrega')
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('pessoa', 'cep', 'logradouro', 'numero', 'complemento', 'bairro', 'municipio', 'UF', 'pais', 'referencia')
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = ('pedido', 'produto', 'qtde', 'preco')
+
+@admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'descricao')  # Campos exibidos na lista
-    list_filter = ('categoria',)  # Filtros na lateral
-    search_fields = ('nome', 'descricao')  # Campo de pesquisa
+    list_display = ('nome', 'cod_barras', 'preco', 'descricao', 'tamanho', 'avaliacao', 'categoria')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'filtros')
--- scavenv/sca/aplic/models.py
+++ scavenv/sca/aplic/models.py
@@ -1,8 +1,89 @@
 from django.db import models
 # Create your models here.
 
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    saldo_conta = models.DecimalField(max_digits=10, decimal_places=2)
+
+class PessoaFisica(Pessoa):
+    cpf = models.CharField(max_length=14)
+    data_nascimento = models.DateField()
+    sexo = models.CharField(max_length=1)
+    rg = models.CharField(max_length=20)
+
+class PessoaJuridica(Pessoa):
+    cnpj = models.CharField(max_length=18)
+    titulo_estabelecimento = models.CharField(max_length=100)
+    codigo_descricao_atividade_economica = models.CharField(max_length=10)
+
+class Telefone(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    numero = models.CharField(max_length=20)
+
+class Usuario(models.Model):
+    pessoa = models.OneToOneField(Pessoa, on_delete=models.CASCADE)
+    senha = models.CharField(max_length=100)
+    email = models.EmailField()
+    favoritos = models.ManyToManyField('Produto')
+    perfil = models.CharField(max_length=100)
+    numero_usuario = models.IntegerField()
+
+class Cartao(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    numero_cartao = models.CharField(max_length=16)
+    nome_impresso = models.CharField(max_length=100)
+    data_vencimento = models.DateField()
+
+class Pedido(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    data = models.DateField()
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_desconto = models.DecimalField(max_digits=10, decimal_places=2)
+
+class HistoricoStatus(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    data = models.DateField()
+
+class Status(models.Model):
+    nome = models.CharField(max_length=100)
+
+class InformacaoEnvio(models.Model):
+    pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE)
+    valor_frete = models.DecimalField(max_digits=10, decimal_places=2)
+    forma_envio = models.CharField(max_length=100)
+    prazo_entrega = models.IntegerField()
+
+class Endereco(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    cep = models.CharField(max_length=10)
+    logradouro = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    complemento = models.CharField(max_length=100, blank=True, null=True)
+    bairro = models.CharField(max_length=100)
+    municipio = models.CharField(max_length=100)
+    UF = models.CharField(max_length=2)
+    pais = models.CharField(max_length=100)
+    referencia = models.CharField(max_length=100, blank=True, null=True)
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.ForeignKey('Produto', on_delete=models.CASCADE)
+    qtde = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
-    descricao = models.TextField()
+    cod_barras = models.CharField(max_length=20)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    # Outros campos do modelo
+    descricao = models.TextField()
+    tamanho = models.CharField(max_length=10)
+    indicacao_uso = models.TextField()
+    avaliacao = models.DecimalField(max_digits=3, decimal_places=2)
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    filtros = models.TextField()
