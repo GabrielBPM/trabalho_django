edce1fc980c6491ea250932fd6ecdd487a04e121
GabrielBPM <gabrielbpmachado@gmail.com>
2023-10-09 13:30:27 -0300

    Adição de campos obrigatórios

--- scavenv/sca/aplic/models.py
+++ scavenv/sca/aplic/models.py
@@ -2,67 +2,67 @@ from django.db import models
 # Create your models here.
 
 class Pessoa(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100, null=False)
     saldo_conta = models.DecimalField(max_digits=10, decimal_places=2)
 
 class PessoaFisica(Pessoa):
-    cpf = models.CharField(max_length=14)
-    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=14, null=False)
+    data_nascimento = models.DateField(null=False)
     sexo = models.CharField(max_length=1)
-    rg = models.CharField(max_length=20)
+    rg = models.CharField(max_length=20, null=False)
 
 class PessoaJuridica(Pessoa):
-    cnpj = models.CharField(max_length=18)
-    titulo_estabelecimento = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=18, null=False)
+    titulo_estabelecimento = models.CharField(max_length=100, null=False)
     codigo_descricao_atividade_economica = models.CharField(max_length=10)
 
 class Telefone(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
-    numero = models.CharField(max_length=20)
+    numero = models.CharField(max_length=20, null=False)
 
 class Usuario(models.Model):
     pessoa = models.OneToOneField(Pessoa, on_delete=models.CASCADE)
-    senha = models.CharField(max_length=100)
-    email = models.EmailField()
+    senha = models.CharField(max_length=100, null=False)
+    email = models.EmailField(null=False)
     favoritos = models.ManyToManyField('Produto')
     perfil = models.CharField(max_length=100)
-    numero_usuario = models.IntegerField()
+    numero_usuario = models.IntegerField(null=False)
 
 class Cartao(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
-    numero_cartao = models.CharField(max_length=16)
-    nome_impresso = models.CharField(max_length=100)
-    data_vencimento = models.DateField()
+    numero_cartao = models.CharField(max_length=16, null=False)
+    nome_impresso = models.CharField(max_length=100, null=False)
+    data_vencimento = models.DateField(null=False)
 
 class Pedido(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
-    data = models.DateField()
-    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField(null=False)
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     valor_desconto = models.DecimalField(max_digits=10, decimal_places=2)
 
 class HistoricoStatus(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
-    data = models.DateField()
+    data = models.DateField(null=False)
 
 class Status(models.Model):
     nome = models.CharField(max_length=100)
 
 class InformacaoEnvio(models.Model):
     pedido = models.OneToOneField(Pedido, on_delete=models.CASCADE)
-    valor_frete = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_frete = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     forma_envio = models.CharField(max_length=100)
-    prazo_entrega = models.IntegerField()
+    prazo_entrega = models.IntegerField(null=False)
 
 class Endereco(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
-    cep = models.CharField(max_length=10)
-    logradouro = models.CharField(max_length=100)
-    numero = models.CharField(max_length=10)
+    cep = models.CharField(max_length=10, null=False)
+    logradouro = models.CharField(max_length=100, null=False)
+    numero = models.CharField(max_length=10, null=False)
     complemento = models.CharField(max_length=100, blank=True, null=True)
-    bairro = models.CharField(max_length=100)
-    municipio = models.CharField(max_length=100)
-    UF = models.CharField(max_length=2)
-    pais = models.CharField(max_length=100)
+    bairro = models.CharField(max_length=100, null=False)
+    municipio = models.CharField(max_length=100, null=False)
+    UF = models.CharField(max_length=2, null=False)
+    pais = models.CharField(max_length=100, null=False)
     referencia = models.CharField(max_length=100, blank=True, null=True)
 
 class ItemPedido(models.Model):
@@ -72,16 +72,16 @@ class ItemPedido(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    cod_barras = models.CharField(max_length=20)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    nome = models.CharField(max_length=100, null=False)
+    cod_barras = models.CharField(max_length=20, null=False)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     descricao = models.TextField()
     tamanho = models.CharField(max_length=10)
-    indicacao_uso = models.TextField()
-    avaliacao = models.DecimalField(max_digits=3, decimal_places=2)
+    indicacao_uso = models.TextField(null=False)
+    avaliacao = models.DecimalField(max_digits=3, decimal_places=2, null=False)
     categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
+    nome = models.CharField(max_length=100, null=False)
+    descricao = models.TextField(null=False)
     filtros = models.TextField()
